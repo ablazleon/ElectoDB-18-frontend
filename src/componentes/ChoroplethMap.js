2d96a9c68ddeea991454b6dc7d0877b093aa3947
JuliaGmP <36381565+JuliaGmP@users.noreply.github.com>
2019-04-15 10:04:15 +0200

    Mapa googlecharts

--- src/componentes/ChoroplethMap.js
+++ src/componentes/ChoroplethMap.js
@@ -14,15 +14,7 @@ Maximo() {
     }
 }
 
-const data = [
-  ["Country", "Popularity"],
-  ["Germany", 200],
-  ["United States", 300],
-  ["Brazil", 400],
-  ["Canada", 500],
-  ["France", 600],
-  ["RU", 700]
-];
+
 
 class ChoroplethMap extends Component {
 
