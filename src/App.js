575674ed0fab44430f4804819f50ce84b0b66809
lingfengzheng <36408048+lingfengzheng@users.noreply.github.com>
2019-04-29 23:40:19 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { connect } from 'react-redux';
 import Game from './componentes/Game'
 import {dual, cambiaLey, cambiaAno, cambiaRegion} from './redux/actions.js';
+import Header from "./componentes/Header";
 
 
 class App extends Component {
@@ -11,6 +12,11 @@ class App extends Component {
      //console.log(this.props.resultados[0][0]);
      //console.log(this.props.currentAno)
      //console.log(this.props.currentRegion)
+let resultados= "Madrid " +"PP: "+ this.props.resultados[this.props.currentAno][0]["PP"][0]+
+                " PSOE: "+this.props.resultados[this.props.currentAno][0]["PSOE"][0]+
+                " Podemos"+this.props.resultados[this.props.currentAno][0]["Podemos"][0]+
+                " Ciu: "+this.props.resultados[this.props.currentAno][0]["CiU"][0] +
+                "Color ganador: "+this.props.resultados[this.props.currentAno][0]["color"];
       return (
       <div className = "Aplicacion">
         <Game resultado={this.props.resultados[this.props.currentAno][this.props.currentRegion]}
@@ -20,8 +26,6 @@ class App extends Component {
               indexAno={this.props.currentAno}
               onChangeLey={ (id)=> {this.props.dispatch(cambiaLey(id))}}
               onChangeAno={ (buttonName) => {this.props.dispatch(cambiaAno(buttonName))}}/>
-
-
       </div>
       );
     }
