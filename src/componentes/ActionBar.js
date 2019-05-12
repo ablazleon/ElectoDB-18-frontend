0a57fa83e4e6ea2a7130e2f45d687581c56dda91
JCastur <julian.castro.turga@alumnos.upm.es>
2019-05-12 18:33:55 +0200

    SelectorNacional añadido

--- src/componentes/ActionBar.js
+++ src/componentes/ActionBar.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Boton from './Boton.js'
 import Selector from './Selector.js'
+import SelectorNacional from './SelectorNacional.js'
+
 export default class ActionBar extends React.Component {
     render(){
 
         return(
+
             <div className="ActionBar">
+
+            <SelectorNacional resultados={this.props.resultados}
+                      currentAno={this.props.currentAno}
+                      currentLey={this.props.currentLey}
+            />
             <Boton buttonName="2019" onChangeAno={this.props.onChangeAno}/>
             <Boton buttonName="2016" onChangeAno={this.props.onChangeAno}/>
             <Boton buttonName="2015" onChangeAno={this.props.onChangeAno}/>
